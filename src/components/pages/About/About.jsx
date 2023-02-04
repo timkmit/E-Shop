5321b51519840e8db4e4e9370a9f51b2bc4772b4
Timur <timengalytchev@mail.ru>
2023-02-04 08:02:54 +0300

    makingRouting

---
+++ src/components/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const About = () => {
+    return ( 
+        <Typography>
+            123
+        </Typography>
+     );
+}
+ 
+export default About;
---
+++ src/components/pages/Faq/Faqq.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Faqq = () => {
+    return ( 
+        <Typography>
+            faq
+        </Typography>
+     );
+}
+ 
+export default Faqq;
